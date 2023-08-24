2f712d8f09f1c479562c3fed45941676a2ae9ffc
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-08-24 10:41:26 +0700

    Create J03012 - TỔNG SỐ NGUYÊN LỚN - 1.java

---
+++ J03012 - TỔNG SỐ NGUYÊN LỚN - 1.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            BigInteger a = sc.nextBigInteger();
+            BigInteger b = sc.nextBigInteger();
+            System.out.println(a.add(b));
+        }
+        sc.close();
+    }
+}
+
+
+
