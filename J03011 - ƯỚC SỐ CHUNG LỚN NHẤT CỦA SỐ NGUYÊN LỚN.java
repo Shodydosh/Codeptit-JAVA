c20f4f0b4128a089d9fdb3bc9cf49e22bb289272
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-08-24 10:55:11 +0700

    Create J03011 - ƯỚC SỐ CHUNG LỚN NHẤT CỦA SỐ NGUYÊN LỚN.java

---
+++ J03011 - ƯỚC SỐ CHUNG LỚN NHẤT CỦA SỐ NGUYÊN LỚN.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            BigInteger a = sc.nextBigInteger();
+            BigInteger b = sc.nextBigInteger();
+            BigInteger ans = a.gcd(b);
+            System.out.println((ans));
+        }
+    }
+}
+
+
+
