5f220b96f9073c0ac99d0938ca1671fdbb848f30
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-09-07 07:04:13 +0700

    Create J01008 - PHÂN TÍCH THỪA SỐ NGUYÊN TỐ.java

---
+++ J01008 - PHÂN TÍCH THỪA SỐ NGUYÊN TỐ.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void solve(int n) {
+        int cnt = 0;
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
+                while(n % i ==0){
+                    n/=i;
+                    cnt++;
+                }
+            }
+            if(cnt != 0){
+                System.out.print(i + "(" + cnt + ") ");
+                cnt = 0;
+            }
+        }
+        if(n != 1) {
+            System.out.print(n + "(" + 1 + ") ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=1; i<= t; i++){
+            int n = sc.nextInt();
+            System.out.print("Test " + i + ": ");
+            solve(n);
+            System.out.println();
+        }
+    }
+}
